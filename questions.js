526a1d35bf74f1fba9f574c5bcbeb170de6553ce
sok0401 <d12371635@gm.tut.edu.tw>
2026-03-04 21:28:51 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,13 @@
+// 這是你的後台題庫，方便隨時修改題目與圖片路徑
+const quizData = [
+    {
+        title: "當你迷失在森林中，看見北極星時你的第一直覺是？",
+        image: "https://via.placeholder.com/600x200/4834d4/ffffff?text=Star+Path", // 這裡放入題目圖片網址
+        options: ["緊跟星光前進", "觀察星象再決定", "尋找身邊的同伴"]
+    },
+    {
+        title: "在命盤的宮位中，你最在意哪一個？",
+        image: "https://via.placeholder.com/600x200/686de0/ffffff?text=Palace+Focus",
+        options: ["命宮", "財帛宮", "夫妻宮"]
+    }
+];
